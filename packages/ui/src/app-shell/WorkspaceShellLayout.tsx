fc84c2eb0ecdf036ae5c704c9ef24c5cca3d0618
Avinaba Das <71009876+mars-s@users.noreply.github.com>
2026-09-25 22:44:00 +1000

    mars: brand workspace shell

--- packages/ui/src/app-shell/WorkspaceShellLayout.tsx
+++ packages/ui/src/app-shell/WorkspaceShellLayout.tsx
@@ -1509,7 +1509,7 @@ export const WorkspaceShellLayout = memo(function WorkspaceShellLayoutComponent(
 
   return (
     <DesktopWindowFrame
-      title={`ZCode / ${getPathLeaf(workspaceAbsPath)}`}
+      title={`Mars / ${getPathLeaf(workspaceAbsPath)}`}
       showHeader
       isDesktop={isDesktop}
       isMacDesktop={isMacDesktop}
